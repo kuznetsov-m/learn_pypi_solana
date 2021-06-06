83be5780e4f7c4801ad9a5cbbaad0fd272621790
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-06 20:22:52 +0300

    transfer ownership of token_x_account to test_program

--- 2_token_transfer_ownership.py
+++ 2_token_transfer_ownership.py
@@ -8,6 +8,7 @@ from solana.system_program import SYS_PROGRAM_ID
 from solana.transaction import AccountMeta, Transaction, TransactionInstruction
 from solana.rpc.types import TxOpts
 from spl.token._layouts import ACCOUNT_LAYOUT
+from spl.token.instructions import AuthorityType
 from solana.account import Account
 from solana.rpc.api import Client
 from solana.publickey import PublicKey
@@ -26,34 +27,38 @@ if initializer_balance == 0:
 
 print(f'initializer_account: {initializer_account.public_key()} balance: {initializer_balance}')
 
+# #####################
+# create token_x_account (owner: initializer_account)
+token_x_mint_account = Account()
 
-######################
-# Program state account creation
+token_x = Token.create_mint(
+    client,
+    payer=initializer_account,
+    mint_authority=token_x_mint_account.public_key(),
+    decimals=8,
+    program_id=TOKEN_PROGRAM_ID
+)
+print(f'token_x mint pubkey: {token_x.pubkey}')
+
+token_x_account_pubkey = token_x.create_account(
+    owner=initializer_account.public_key()
+)
+print(f'token_x_account_pubkey: {token_x_account_pubkey}')
+
+# ######################
+# transfer ownership of token_x_account to test_program
 
 test_program_keypair = [42,126,209,101,182,11,110,103,64,11,125,3,77,237,86,140,99,131,0,181,152,37,25,20,74,220,91,92,170,24,220,176,31,42,156,100,224,241,175,248,175,59,57,167,219,153,17,228,40,106,38,177,134,107,203,228,147,122,71,65,0,219,199,143]
 test_program_account = Account(test_program_keypair[:32])
 
-program_sub_account = Account()
-print(f'program_sub_account pubkey: {program_sub_account.public_key()}')
-# Allocate memory for the account
-balance_needed = client.get_minimum_balance_for_rent_exemption(ACCOUNT_LAYOUT.sizeof()).get('result')
-# Construct transaction
-txn = Transaction()
-txn.add(
-    sp.create_account(
-        sp.CreateAccountParams(
-            from_pubkey=initializer_account.public_key(),
-            new_account_pubkey=program_sub_account.public_key(),
-            lamports=balance_needed,
-            space=ACCOUNT_LAYOUT.sizeof(),
-            program_id=test_program_account.public_key(),
-        )
-    )
+rpc_response = token_x.set_authority_custum(
+    account=token_x_account_pubkey,
+    current_authority=initializer_account.public_key(),
+    authority_type=AuthorityType.ACCOUNT_OWNER
 )
-
-rpc_response = client.send_transaction(
-    txn,
-    initializer_account, program_sub_account,
-    opts=TxOpts(skip_preflight=True, skip_confirmation=False)
-)
+# initializer_account: EjgZsApz7CFqSHPqxKS7UYXdmnEP39UviXo5D6TWLhDM balance: 978297040
+# token_x mint pubkey: CjrH8m1AJupDV4botFX1ioS8mh1htfUbtKv8ZvAuj7DV
+# token_x_account_pubkey: HDWJUvic6MHsaU2keEAkBXNHtPZHADNZ2Um8joqcW5QA
+# Error:
+# {'code': -32002, 'message': 'Transaction simulation failed: Error processing Instruction 0: custom program error: 0xc', 'data': {'err': {'InstructionError': [0, {'Custom': 12}]}, 'logs': ['Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [1]', 'Program log: Instruction: SetAuthority', 'Program log: Error: Invalid instruction', 'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 2021 of 200000 compute units', 'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA failed: custom program error: 0xc']}}
 print(f'rpc_response: {rpc_response}')
